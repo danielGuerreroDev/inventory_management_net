a0f4628db5507fbd36f9bbc8d60fff2e416955da
danoGuerrero <danielguerrerodev@gmail.com>
2023-07-04 21:21:56 -0600

    API consume test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 const App = () => {
-  return <h1>Hello React DEV 333</h1>;
+  const [data, setData] = useState(null);
+
+  const getData = () => {
+    Axios.get('https://inventory-management-q6zw.onrender.com/getProducts').then(res => {
+      setData(res.data);
+    })
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const products = data?.map((item) => (
+    <li key={item.id}>
+      <i>{item.title}</i>
+    </li>
+  ));
+
+  return products;
 };
 
 export default App;
