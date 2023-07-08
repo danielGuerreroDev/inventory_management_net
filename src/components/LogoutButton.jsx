c64f216b695d0ec2215e05381de8b7c024280176
danoGuerrero <danielguerrerodev@gmail.com>
2023-07-08 01:23:42 -0600

    _redirects index

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -6,7 +6,7 @@ function LogoutButton() {
   const { logout } = useAuth0();
 
   const logoutClick = () => {
-    logout({ logoutParams: { returnTo:"http://localhost:3000" } });
+    logout({ logoutParams: { returnTo:"https://prod--im-danielguerrero.netlify.app/" } });
   }
 
   return (
