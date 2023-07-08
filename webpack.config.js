d79ef405285ff6dd50755ac8620a25513019ecbc
danoGuerrero <danielguerrerodev@gmail.com>
2023-07-08 00:58:35 -0600

    _redirects index

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   output: {
-    path: path.join(__dirname, "/"), // the bundle output path
+    path: path.join(__dirname, "/src/build"), // the bundle output path
     filename: "bundle.js", // the name of the bundle
     publicPath: '/'
   },
